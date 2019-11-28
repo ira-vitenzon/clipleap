1ef5dff7c0f9e4299e2c068db02956a0cf5c1a16
Dvir <>
2019-11-28 17:22:03 +0200

    Test

--- app/src/test/java/com/joyfulshark/clipleap/ExampleUnitTest.java
+++ app/src/test/java/com/joyfulshark/clipleap/ExampleUnitTest.java
@@ -1,6 +1,14 @@
 package com.joyfulshark.clipleap;
 
+import android.content.res.AssetManager;
+
+import com.joyfulshark.clipleap.common.ObjectsScores;
+
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 import static org.junit.Assert.*;
 
@@ -14,4 +22,13 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void csvTest() throws FileNotFoundException {
+        File file = new File("C:\\Users\\Dvir\\Desktop\\Android\\clipleap\\app\\src\\main\\assets\\objects.csv");
+        FileReader fileReader = new FileReader(file);
+        ObjectsScores.getInstance().build(fileReader);
+        System.out.println(ObjectsScores.getObjectsMap());
+    }
+
 }
